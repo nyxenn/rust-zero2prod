e4555442d4124150c350242e7c5196fd00291498
Nyxenn <jordib.edu@gmail.com>
2023-04-30 14:25:21 +0200

    First endpoint + test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8090").expect("Failed to bind to address");
+    run(listener)?.await
 }
